b16fe5103214777f0d092074b6e4a8b36bad9f10
Suprcheese <Suprcheese@users.noreply.github.com>
2016-03-27 00:22:25 -0700

    Increased default difficulty settings: all EMP attacks have increased disable duration. To compensate, gun turrets are now disabled by EMP for only half as long as other entities. This should encourage using mixed defenses.

--- Readme.txt
+++ Readme.txt
@@ -1,11 +1,13 @@
-EMP Biters 1.0.0
+EMP Biters 1.0.1
 ================
 
-Version 1.0.0 was released March 26, 2016, was tested using Factorio v0.12.29, and was authored by Supercheese.
+Version 1.0.1 was released March 27, 2016, was tested using Factorio v0.12.29, and was authored by Supercheese.
 
 Many mods give the player a whole host of new weapons and defenses to play with, but the poor biters are left behind. It's time they received an upgrade!
 Now, large biters have a chance to give off an EMP blast when they die, temporarily disabling nearby turrets, radars, and logistic & construction robots. Large spitters also have a chance to EMP with their spit.
 The larger the enemy and the higher the evolution factor, the bigger the chance of EMP is.
+
+Gun turrets are only disabled by EMP for half as long as laser turrets, to encourage using mixed defenses.
 Big Worms have a 100% chance to cause a large EMP blast with their spit, to make turret creep more challenging.
 
 This mod also supports Bob's Enemies, Dytech War, and Natural Evolution Enemies.
--- config.lua
+++ config.lua
@@ -14,34 +14,34 @@
 EMPBiterTable = {}
 
 -- Vanilla Biters
-EMPBiterTable["big-biter"] = 							{10, 15,  180, 300}
-EMPBiterTable["behemoth-biter"] = 						{15, 25,  240, 600}
+EMPBiterTable["big-biter"] = 							{10, 15,  300, 600}
+EMPBiterTable["behemoth-biter"] = 						{15, 25,  300, 900}
 
 -- Bob's Enemies
-EMPBiterTable["bob-bigger-biter"] =						{10, 20,  300, 600}
-EMPBiterTable["bob-biggest-biter"] =					{12, 25,  360, 900}
-EMPBiterTable["bob-fire-biter"] =						{14, 30,  420, 900}
-EMPBiterTable["bob-titan-biter"] =						{16, 35,  600, 1200}
-EMPBiterTable["bob-behemoth-biter"] =					{18, 40,  600, 1500}
+EMPBiterTable["bob-bigger-biter"] =						{10, 20,  360, 900}
+EMPBiterTable["bob-biggest-biter"] =					{12, 25,  600, 900}
+EMPBiterTable["bob-fire-biter"] =						{14, 30,  600, 1200}
+EMPBiterTable["bob-titan-biter"] =						{16, 35,  900, 1500}
+EMPBiterTable["bob-behemoth-biter"] =					{18, 40,  1200, 2400}
 
 -- Dytech War
-EMPBiterTable["teen-berserk-biter"] = 					{10, 10,  180, 300}
-EMPBiterTable["teen-elder-biter"] = 					{11, 15,  180, 240}
-EMPBiterTable["teen-king-biter"] = 						{12, 20,  300, 600}
-EMPBiterTable["teen-queen-biter"] = 					{13, 20,  300, 600}
-EMPBiterTable["adult-berserk-biter"] = 					{14, 20,  300, 720}
-EMPBiterTable["adult-elder-biter"] = 					{15, 25,  420, 900}
-EMPBiterTable["adult-king-biter"] = 					{20, 50,  600, 900}
-EMPBiterTable["adult-queen-biter"] = 					{25, 100, 600, 1200}
+EMPBiterTable["teen-berserk-biter"] = 					{10, 10,  300, 600}
+EMPBiterTable["teen-elder-biter"] = 					{11, 15,  300, 900}
+EMPBiterTable["teen-king-biter"] = 						{12, 20,  300, 1200}
+EMPBiterTable["teen-queen-biter"] = 					{13, 20,  300, 1500}
+EMPBiterTable["adult-berserk-biter"] = 					{14, 20,  600, 1200}
+EMPBiterTable["adult-elder-biter"] = 					{15, 25,  600, 1500}
+EMPBiterTable["adult-king-biter"] = 					{20, 50,  900, 2100}
+EMPBiterTable["adult-queen-biter"] = 					{25, 100, 900, 2400}
 
-EMPBiterTable["dyzilla-unit-1"] = 						{20, 100, 600, 900}
-EMPBiterTable["dyzilla-unit-2"] = 						{25, 100, 600, 1200}
-EMPBiterTable["dyzilla-unit-3"] = 						{40, 100, 900, 1200}
-EMPBiterTable["dyzilla-unit-4"] = 						{50, 100, 900, 1800}
+EMPBiterTable["dyzilla-unit-1"] = 						{20, 100, 1200, 1800}
+EMPBiterTable["dyzilla-unit-2"] = 						{25, 100, 1200, 2100}
+EMPBiterTable["dyzilla-unit-3"] = 						{40, 100, 1200, 2400}
+EMPBiterTable["dyzilla-unit-4"] = 						{50, 100, 1200, 3000}
 
 -- Natural Evolution Enemies
-EMPBiterTable["big-biter-Mk2"] = 						{12, 20,  240, 420}
-EMPBiterTable["big-biter-Mk3"] = 						{14, 20,  300, 600}
+EMPBiterTable["big-biter-Mk2"] = 						{12, 20,  300, 600}
+EMPBiterTable["big-biter-Mk3"] = 						{14, 20,  300, 900}
 
 
 -- This is the lookup table for EMP spitters & worms. Rather than add its own custom spitters, this mod gives certain extant spitters a chance to disable a turret when they hit it with their spit.
@@ -52,24 +52,24 @@ EMPBiterTable["big-biter-Mk3"] = 						{14, 20,  300, 600}
 EMPSpitterTable = {}
 
 -- Vanilla Spitters
-EMPSpitterTable["EMP-acid-splash-purple-big"] =			{1,  20,  180, 300}
-EMPSpitterTable["EMP-acid-splash-purple-behemoth"] =	{2,  25,  240, 600}
+EMPSpitterTable["EMP-acid-splash-purple-big"] =			{1,  20,  300, 600}
+EMPSpitterTable["EMP-acid-splash-purple-behemoth"] =	{2,  25,  300, 900}
 
 -- Vanilla Big Worm
 EMPSpitterTable["EMP-acid-splash-purple-worm"] =		{20, 100, 600, 1200} -- Suck it, turret creep!
 
 -- Bob's Enemies
-EMPSpitterTable["EMP-acid-splash-explosive"] =			{1,  20,  180, 300}  -- "bob-bigger-spitter" and "bob-big-explosive-worm-turret"
-EMPSpitterTable["EMP-acid-splash-fire"] =				{2,  20,  240, 420}  -- "bob-biggest-spitter" and "bob-big-fire-worm-turret"
+EMPSpitterTable["EMP-acid-splash-explosive"] =			{1,  20,  300, 420}  -- "bob-bigger-spitter" and "bob-big-explosive-worm-turret"
+EMPSpitterTable["EMP-acid-splash-fire"] =				{2,  20,  300, 540}  -- "bob-biggest-spitter" and "bob-big-fire-worm-turret"
 EMPSpitterTable["EMP-acid-splash-poison"] =				{3,  20,  420, 600}  -- "bob-poison-spitter" and "bob-big-poison-worm-turret"
-EMPSpitterTable["EMP-acid-splash-titan"] =				{4,  25,  540, 900}  -- "bob-titan-spitter"
-EMPSpitterTable["EMP-acid-splash-behemoth"] =			{5,  25,  600, 1200} -- "bob-behemoth-spitter"
+EMPSpitterTable["EMP-acid-splash-titan"] =				{4,  25,  600, 900}  -- "bob-titan-spitter"
+EMPSpitterTable["EMP-acid-splash-behemoth"] =			{5,  25,  900, 1200} -- "bob-behemoth-spitter"
 
 -- Dytech War
-EMPSpitterTable["EMP-acid-splash-berserker"] =			{2,  25,  180, 420}  -- All Berzerker-type spitters
-EMPSpitterTable["EMP-acid-splash-elder"] =				{3,  40,  300, 600}  -- All Elder-type spitters
-EMPSpitterTable["EMP-acid-splash-king"] =				{5,  50,  300, 900}  -- All King-type spitters
+EMPSpitterTable["EMP-acid-splash-berserker"] =			{2,  25,  300, 600}  -- All Berzerker-type spitters
+EMPSpitterTable["EMP-acid-splash-elder"] =				{3,  40,  600, 900}  -- All Elder-type spitters
+EMPSpitterTable["EMP-acid-splash-king"] =				{5,  50,  600, 1200}  -- All King-type spitters
 
 -- Natural Evolution Enemies
-EMPSpitterTable["EMP-acid-splash-infected"] =			{2,  25,  180, 420}  -- "big-spitter-Mk2"
-EMPSpitterTable["EMP-acid-splash-mutated"] =			{2,  25,  180, 600}  -- "big-spitter-Mk3"
+EMPSpitterTable["EMP-acid-splash-infected"] =			{2,  25,  300, 720}  -- "big-spitter-Mk2"
+EMPSpitterTable["EMP-acid-splash-mutated"] =			{2,  25,  300, 900}  -- "big-spitter-Mk3"
